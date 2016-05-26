f2c6d2152af4f3f26f99c4d04b7ff28d36d207a1
Byunghyun-P <park@byunghyun.me>
2016-05-26 23:30:20 +0900

    1. 캐릭터 생성 (Unity Asset Store 무료 Asset 사용) 2. 클릭시 한칸 앞으로 이동 3. 클릭시 캐릭터의 Run animation 실행 4. 이동 완료시 캐릭터의 Run animation 종료

---
+++ unity/Assets/script/movement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+	Animator _animator;
+	// Use this for initialization
+	void Start () {
+		_animator = gameObject.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began) {
+		if (_animator.GetInteger("Status") == 0 && Input.GetMouseButtonDown(0)){
+			Debug.Log ("Clicked");
+			transform.Translate(Vector3.forward);
+			_animator.SetInteger("Status",2);
+		} else if (transform.hasChanged) {
+			Debug.Log ("Changed");
+			transform.hasChanged = false;
+			_animator.SetInteger("Status",0);
+		}
+
+	}
+}
